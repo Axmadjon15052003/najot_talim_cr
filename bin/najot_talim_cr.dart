0b93f246ed0ba11a49745ccc2043cadb5c1e307a
Ahmadjon <axmadjonbakirov2@gmail.com>
2023-09-26 18:47:45 +0500

    hamma fayllarni gitga qo'shdik

---
+++ bin/najot_talim_cr.dart
@@ -0,0 +1,5 @@
+import 'package:najot_talim_cr/najot_talim_cr.dart' as najot_talim_cr;
+
+void main(List<String> arguments) {
+  print('Hello world: ${najot_talim_cr.calculate()}!');
+}
---
+++ test/najot_talim_cr_test.dart
@@ -0,0 +1,8 @@
+import 'package:najot_talim_cr/najot_talim_cr.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
